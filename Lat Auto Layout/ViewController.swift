ed04d091bcb8e2b97e8317d57e3ad391e3eb7357
Dzulfikar Fauzi <dzulfikar68.fauzi@gmail.com>
2024-04-24 10:45:07 +0700

    Initial Commit

---
+++ Lat Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lat Auto Layout
+//
+//  Created by Dzulfikar Fauzi on 24/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
